53edea5e1da5f4ed6218c48160a10516239643fb
kazanik91 <plhooper91@gmail.com>
2023-08-27 01:14:49 +0200

    Astra database connection configuration

--- src/main/java/pl/kazanik/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ src/main/java/pl/kazanik/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -1,13 +1,27 @@
 package pl.kazanik.betterreadsdataloader;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import pl.kazanik.betterreadsdataloader.connection.DatastaxAstraProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DatastaxAstraProperties.class)
 public class BetterreadsDataLoaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DatastaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 
 }
